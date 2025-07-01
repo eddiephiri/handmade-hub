141ad0596df5897f722de3a77bdf0f8c09f1e019
Your Name <user@example.com>
2025-07-01 18:20:55 +0200

    fix: remove duplicate navbar

--- lib/handmade_hub_web/controllers/page_controller.ex
+++ lib/handmade_hub_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule HandmadeHubWeb.PageController do
   use HandmadeHubWeb, :controller
 
   def home(conn, _params) do
-    Phoenix.LiveView.Controller.live_render(conn, HandmadeHubWeb.BrowseLive.Index, session: %{})
+    Phoenix.Controller.redirect(conn, to: "/browse")
   end
 end
